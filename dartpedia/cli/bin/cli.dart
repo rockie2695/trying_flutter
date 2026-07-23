f588a889cf320467ed38778221f0b197bf91c7e9
“Aidan <aidanchoi@arpainfinity.com>
2026-07-23 19:20:58 +0800

    feat: package 1

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,5 +1,7 @@
 import 'dart:io'; // Add this line at the top
 import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
 
 const version = '0.0.1'; // Add this line
 
